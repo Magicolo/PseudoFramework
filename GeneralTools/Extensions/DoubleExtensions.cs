330026da31b0d35b5a7f2c67d4f580e2187fa3e5
Magicolo <straty91@hotmail.com>
2015-12-04 00:10:02 -0500

    -Fixed spaces

--- GeneralTools/Extensions/DoubleExtensions.cs
+++ GeneralTools/Extensions/DoubleExtensions.cs
@@ -4,83 +4,83 @@ using System.Collections;
 
 namespace Pseudo
 {
-    public static class DoubleExtensions
-    {
-        public static double Scale(this double d, double sourceMin, double sourceMax, double targetMin, double targetMax)
-        {
-            return (d - sourceMin) / (sourceMax - sourceMin) * (targetMax - targetMin) + targetMin;
-        }
+	public static class DoubleExtensions
+	{
+		public static double Scale(this double d, double sourceMin, double sourceMax, double targetMin, double targetMax)
+		{
+			return (d - sourceMin) / (sourceMax - sourceMin) * (targetMax - targetMin) + targetMin;
+		}
 
-        public static double PowSign(this double d, double power)
-        {
-            return Math.Abs(d).Pow(power) * d.Sign();
-        }
+		public static double PowSign(this double d, double power)
+		{
+			return Math.Abs(d).Pow(power) * d.Sign();
+		}
 
-        public static double Pow(this double d, double power)
-        {
-            if (double.IsNaN(d))
-                return 0;
+		public static double Pow(this double d, double power)
+		{
+			if (double.IsNaN(d))
+				return 0;
 
-            if (power == 0d)
-                return 1;
+			if (power == 0d)
+				return 1;
 
-            if (power == 1d)
-                return d;
+			if (power == 1d)
+				return d;
 
-            if (power == 2d)
-                return d * d;
+			if (power == 2d)
+				return d * d;
 
-            if (power == 3d)
-                return d * d * d;
+			if (power == 3d)
+				return d * d * d;
 
-            return Math.Pow(d, power);
-        }
+			return Math.Pow(d, power);
+		}
 
-        public static double Round(this double d, double step)
-        {
-            if (double.IsNaN(d))
-                return 0d;
+		public static double Round(this double d, double step)
+		{
+			if (double.IsNaN(d))
+				return 0d;
 
-            if (step <= 0)
-                return d;
+			if (step <= 0)
+				return d;
 
-            if (step == 1d)
-                return Math.Round(d);
+			if (step == 1d)
+				return Math.Round(d);
 
-            return Math.Round(d * (1d / step)) * step;
-        }
+			return Math.Round(d * (1d / step)) * step;
+		}
 
-        public static double Round(this double d)
-        {
-            return d.Round(1d);
-        }
+		public static double Round(this double d)
+		{
+			return d.Round(1d);
+		}
 
-        public static bool IsBetween(this double f, double min, double max)
-        {
-            return f > min && f < max;
-        }
+		public static bool IsBetween(this double f, double min, double max)
+		{
+			return f > min && f < max;
+		}
 
-        public static double Wrap(this double d, double min, double max)
-        {
-            double difference = max - min;
+		public static double Wrap(this double d, double min, double max)
+		{
+			double difference = max - min;
 
-            while (d < min)
-                d += difference;
+			while (d < min)
+				d += difference;
 
-            while (d >= max)
-                d -= difference;
+			while (d >= max)
+				d -= difference;
 
-            return d;
-        }
+			return d;
+		}
 
-        public static int Sign(this double d)
-        {
-            return d >= 0d ? 1 : -1;
-        }
+		public static int Sign(this double d)
+		{
+			return d >= 0d ? 1 : -1;
+		}
 
-        public static double SetSign(this double d, bool sign)
-        {
-            return Math.Abs(d) * sign.Sign();
-        }
-    }
+		public static double SetSign(this double d, bool sign)
+		{
+			return Math.Abs(d) * sign.Sign();
+		}
+	}
 }
--- GeneralTools/Extensions/IntExtensions.cs
+++ GeneralTools/Extensions/IntExtensions.cs
@@ -3,69 +3,69 @@ using UnityEngine;
 
 namespace Pseudo
 {
-    public static class IntExtensions
-    {
-        public static int Scale(this int i, int currentMin, int currentMax, int targetMin, int targetMax)
-        {
-            return (i - currentMin) / (currentMax - currentMin) * (targetMax - targetMin) + targetMin;
-        }
+	public static class IntExtensions
+	{
+		public static int Scale(this int i, int currentMin, int currentMax, int targetMin, int targetMax)
+		{
+			return (i - currentMin) / (currentMax - currentMin) * (targetMax - targetMin) + targetMin;
+		}
 
-        public static float PowSign(this int i, int power)
-        {
-            return Mathf.Abs(i).Pow(power) * i.Sign();
-        }
+		public static float PowSign(this int i, int power)
+		{
+			return Mathf.Abs(i).Pow(power) * i.Sign();
+		}
 
-        public static float Pow(this int i, int power)
-        {
-            if (power == 0)
-                return 1;
+		public static float Pow(this int i, int power)
+		{
+			if (power == 0)
+				return 1;
 
-            if (power == 1)
-                return i;
+			if (power == 1)
+				return i;
 
-            if (power == 2)
-                return i * i;
+			if (power == 2)
+				return i * i;
 
-            if (power == 3)
-                return i * i * i;
+			if (power == 3)
+				return i * i * i;
 
-            return Mathf.Pow(i, power);
-        }
+			return Mathf.Pow(i, power);
+		}
 
-        public static int Round(this int i, int step)
-        {
-            if (step <= 0)
-                return i;
+		public static int Round(this int i, int step)
+		{
+			if (step <= 0)
+				return i;
 
-            return (int)(Math.Round(i * (1d / step)) * step);
-        }
+			return (int)(Math.Round(i * (1d / step)) * step);
+		}
 
-        public static bool IsBetween(this int f, int min, int max)
-        {
-            return f > min && f < max;
-        }
+		public static bool IsBetween(this int f, int min, int max)
+		{
+			return f > min && f < max;
+		}
 
-        public static int Wrap(this int i, int min, int max)
-        {
-            int difference = max - min;
+		public static int Wrap(this int i, int min, int max)
+		{
+			int difference = max - min;
 
-            while (i < min)
-                i += difference;
+			while (i < min)
+				i += difference;
 
-            while (i >= max)
-                i -= difference;
+			while (i >= max)
+				i -= difference;
 
-            return i;
-        }
+			return i;
+		}
 
-        public static int Sign(this int i)
-        {
-            return i >= 0 ? 1 : -1;
-        }
+		public static int Sign(this int i)
+		{
+			return i >= 0 ? 1 : -1;
+		}
 
-        public static int SetSign(this int i, bool sign)
-        {
-            return Mathf.Abs(i) * sign.Sign();
-        }
-    }
+		public static int SetSign(this int i, bool sign)
+		{
+			return Mathf.Abs(i) * sign.Sign();
+		}
+	}
 }
